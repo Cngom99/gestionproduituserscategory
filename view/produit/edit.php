7c4df7805a76a4cfc45d8d52ce841244de49ad66
Ngom <130662339+Cngom99@users.noreply.github.com>
2024-12-17 18:03:49 +0000

    edit.php

--- view/produit/edit.php
+++ view/produit/edit.php
@@ -1,14 +1,18 @@
-<form action="?controller=produit&action=update" method="POST">
-    <input type="hidden" name="id" value="<?= $produit['id'] ?>"><br>
-
-    <label for="libelle">Libelle:</label>
-    <input type="text" name="libelle" value="<?= $produit['libelle'] ?>" required><br>
-
-    <label for="quantite">Quantite:</label>
-    <input type="number" name="quantite" value="<?= $produit['qt'] ?>" required><br>
-
-    <label for="prix">Prix Unitaire:</label>
-    <input type="number" name="prix" value="<?= $produit['pu'] ?>" required><br>
-
-    <button type="submit">Update</button>
+<form action="?controller=produit&&action=update" method="POST">
+    <input type="text" name="id" value="<?= $produit['id'] ?>" hidden><br>
+    <label for="">Libelle</label>
+    <input type="text" name="libelle" value="<?= $produit['libelle'] ?>"><br>
+    <label for="">Quantité</label>
+    <input type="text" name="quantite" value="<?= $produit['qt'] ?>"><br>
+    <label for="">Prix Unitaire</label>
+    <input type="text" name="prix" value="<?= $produit['pu'] ?>"><br>
+    <label for="">Catégorie</label>
+    <select name="idcat">""
+        <?php while($c = pg_fetch_assoc($categories)) { ?>
+            <option value="<?= $c['id'] ?>" <?= $produit['idcat'] == $c['id'] ? 'selected' : '' ?>>
+                <?= $c['libelle'] ?>
+            </option>
+        <?php } ?>
+    </select><br>
+    <button type="submit">Modifier</button>
 </form>
