67b12f0e04596df805f764137c18fa7ef809d337
Ngom <130662339+Cngom99@users.noreply.github.com>
2024-12-17 18:07:49 +0000

    add.php

--- view/user/add.php
+++ view/user/add.php
@@ -1,15 +1,12 @@
+<h1>Ajouter un utilisateur</h1>
 <form method="POST" action="index.php?controller=user&action=save">
-    <label for="nom">Nom:</label>
-    <input type="text" id="nom" name="nom" required><br>
-
-    <label for="prenom">Prénom:</label>
-    <input type="text" id="prenom" name="prenom" required><br>
-
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required><br>
-
-    <label for="motdepasse">Mot de passe:</label>
-    <input type="password" id="mot_de_passe" name="mot_de_passe" required><br>
-
-    <input type="submit" value="Ajouter l'utilisateur">
+    <label>Nom :</label>
+    <input type="text" name="nom" required><br>
+    <label>Prénom :</label>
+    <input type="text" name="prenom" required><br>
+    <label>Email :</label>
+    <input type="email" name="email" required><br>
+    <label>Mot de passe :</label>
+    <input type="password" name="password" required><br>
+    <button type="submit">Enregistrer</button>
 </form>
