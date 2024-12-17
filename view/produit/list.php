f00683cdcdf96fa182b8c74d382ecce88c36d87c
Ngom <130662339+Cngom99@users.noreply.github.com>
2024-12-17 18:05:17 +0000

    list.php

--- view/produit/list.php
+++ view/produit/list.php
@@ -1,22 +1,36 @@
-
-<a href="?controller=produit&&action=add">ADD PRODUCT</a>
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Libelle</th>
-        <th>Quantite</th>
-        <th>Prix Unitaire</th>
-    </tr>
-    <?php while( $p = pg_fetch_assoc($produits)){ ?>
-        <tr>
-            <td><?= $p['id'] ?></td>
-            <td><?= $p['libelle'] ?></td>
-            <td><?= $p['qt'] ?></td>
-            <td><?= $p['pu'] ?></td>
-            <td>
-                <a href="?controller=produit&&action=delete&id=<?= $p['id'] ?>">Delete</a>
-                <a href="?controller=produit&&action=update&id=<?= $p['id'] ?>">Update</a>
-            </td>
-        </tr>
-    <?php } ?>    
-</table>
+<body>
+    <div class="container">
+        <div class="add-product-link">
+            <a href="?controller=produit&&action=add">Ajouter un Produit</a>
+        </div>
+        <table class="table table-bordered table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <!-- <th>ID</th> -->
+                    <th>Libelle</th>
+                    <th>Quantité</th>
+                    <th>Prix Unitaire</th>
+                    <th>Catégorie</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while($p = pg_fetch_assoc($produits)) { ?>
+                    <tr>
+                        <!-- <td><?= $p['id'] ?></td> -->
+                        <td><?= $p['libelle'] ?></td>
+                        <td><?= $p['qt'] ?></td>
+                        <td><?= $p['pu'] ?></td>
+                        <td><?= $p['categorie'] ?></td>
+                        <td>
+                            <a href="?controller=produit&&action=delete&id=<?= $p['id'] ?>" class="btn-actions delete">Supprimer</a>
+                            <a href="?controller=produit&&action=edit&id=<?= $p['id'] ?>" class="btn-actions edit">Modifier</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
